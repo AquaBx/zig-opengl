438bcb06190ebdb93807f31771282d6d32d129ab
Tom Chauvel <tom.chauvel@hotmail.com>
2023-08-12 22:47:42 +0200

    mb

---
+++ build.zig
@@ -0,0 +1,62 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const exe = b.addExecutable(.{
+        .name = "zigrenderer",
+        .root_source_file = .{ .path = "src/main.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const glfw_dep = b.dependency("mach_glfw", .{
+        .target = target,
+        .optimize = optimize,
+    });
+
+    // Now we link the resulting library,
+    // passing in the artifact from our dependency
+    exe.linkLibrary(glfw_dep.artifact("mach-glfw"));
+
+    // Add the module to our package scope
+    // Note the name here is the module that
+    // you will import (`@import("mach-glfw")`)
+    exe.addModule("mach-glfw", glfw_dep.module("mach-glfw"));
+
+    // Use the mach-glfw .link helper here
+    // to link the glfw library for us
+    try @import("mach_glfw").link(b, exe);
+
+    // Same as above for our gl module,
+    // because we copied the gl code into the project
+    // we instead just create the module inline
+    exe.addModule("gl", b.createModule(.{
+        .source_file = .{ .path = "libs/gl46.zig" },
+    }));
+
+    b.installArtifact(exe);
+
+    const run_cmd = b.addRunArtifact(exe);
+
+    run_cmd.step.dependOn(b.getInstallStep());
+
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
+
+    const run_step = b.step("run", "Run the app");
+    run_step.dependOn(&run_cmd.step);
+
+    const unit_tests = b.addTest(.{
+        .root_source_file = .{ .path = "src/main.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const run_unit_tests = b.addRunArtifact(unit_tests);
+
+    const test_step = b.step("test", "Run unit tests");
+    test_step.dependOn(&run_unit_tests.step);
+}
---
+++ src/main.zig
@@ -0,0 +1,120 @@
+const std = @import("std");
+const glfw = @import("mach-glfw");
+const gl = @import("gl");
+
+const log = std.log.scoped(.Engine);
+
+fn glGetProcAddress(p: glfw.GLProc, proc: [:0]const u8) ?gl.FunctionPointer {
+    _ = p;
+    return glfw.getProcAddress(proc);
+}
+
+/// Default GLFW error handling callback
+fn errorCallback(error_code: glfw.ErrorCode, description: [:0]const u8) void {
+    std.log.err("glfw: {}: {s}\n", .{ error_code, description });
+}
+
+fn errorGlCallback(source: gl.GLenum, _type: gl.GLenum, id: gl.GLuint, severity: gl.GLenum, length: gl.GLsizei, message: [*:0]const u8, userParam: ?*anyopaque) callconv(.C) void {
+    _ = source;
+    _ = _type;
+    _ = id;
+    _ = severity;
+    _ = length;
+    _ = userParam;
+
+    std.log.err("gl: {s}\n", .{ message });
+}
+
+pub fn main() !void {
+    glfw.setErrorCallback(errorCallback);
+    if (!glfw.init(.{})) {
+        std.log.err("failed to initialize GLFW: {?s}", .{glfw.getErrorString()});
+        std.process.exit(1);
+    }
+    defer glfw.terminate();
+
+    // Create our window
+    const window = glfw.Window.create(640, 480, "mach-glfw + zig-opengl", null, null, .{
+        .opengl_profile = .opengl_core_profile,
+        .context_version_major = 4,
+        .context_version_minor = 0,
+    }) orelse {
+        std.log.err("failed to create GLFW window: {?s}", .{glfw.getErrorString()});
+        std.process.exit(1);
+    };
+    defer window.destroy();
+
+    glfw.makeContextCurrent(window);
+
+    const proc: glfw.GLProc = undefined;
+    try gl.load(proc, glGetProcAddress);
+
+    // test du polygon
+
+    var a = "#version 330 core\nlayout (location = 0) in vec3 aPos;\nvoid main() {\n    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n}";
+    var b = "#version 330 core\nout vec4 FragColor;\nvoid main()\n{FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n}";
+
+    gl.viewport(0,0,640,480);
+
+    gl.enable(gl.DEBUG_OUTPUT);
+    gl.debugMessageCallback(errorGlCallback,null);
+
+    const vertices = [_]f32{ -0.5,-0.5 * 0.58, 0, 
+                              0.5,-0.5 * 0.58, 0,
+                              0.0, 1.0 * 0.58, 0 
+                            };
+
+    var vertexShaderSource = [_][*:0]const u8 { a };
+    const vertexShader:gl.GLuint = gl.createShader(gl.VERTEX_SHADER);
+    gl.shaderSource(vertexShader, 1, &vertexShaderSource, null);
+    gl.compileShader(vertexShader);
+
+    var fragmentShaderSource =  [_][*:0]const u8 { b };
+    const fragmentShader:gl.GLuint = gl.createShader(gl.FRAGMENT_SHADER);
+    gl.shaderSource(fragmentShader, 1, &fragmentShaderSource, null);
+    gl.compileShader(fragmentShader);
+
+    const shaderProgram:gl.GLuint  = gl.createProgram();
+    gl.attachShader(shaderProgram,vertexShader);
+    gl.attachShader(shaderProgram,fragmentShader);
+
+    gl.linkProgram(shaderProgram);
+    gl.deleteShader(vertexShader);
+    gl.deleteShader(fragmentShader);
+
+    var VAO : gl.GLuint = 0;
+    var VBO : gl.GLuint = 0;
+
+    gl.genVertexArrays(1, &VAO);
+    gl.genBuffers(1,&VBO);
+
+    gl.bindVertexArray(VAO);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER,VBO);
+    gl.bufferData(gl.ARRAY_BUFFER, vertices.len * @sizeOf(f32)  , &vertices, gl.STATIC_DRAW);
+
+    gl.vertexAttribPointer(0,3, gl.FLOAT, gl.FALSE, 3 * @sizeOf(f32), &void ); // 9 = sizeof(float)
+    gl.enableVertexAttribArray(0);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER,0);
+    gl.bindVertexArray(0);
+
+    // Wait for the user to close the window.
+    while (!window.shouldClose()) {
+
+        gl.clearColor(1, 0, 1, 1);
+        gl.clear(gl.COLOR_BUFFER_BIT);
+
+        gl.useProgram(shaderProgram);
+        gl.bindVertexArray(VAO);
+        gl.drawArrays(gl.TRIANGLES,0,3);
+
+        window.swapBuffers();
+        glfw.pollEvents();
+    }
+
+    gl.deleteVertexArrays(1,&VAO);
+    gl.deleteBuffers(1,&VBO);
+    gl.deleteProgram(shaderProgram);
+
+}
